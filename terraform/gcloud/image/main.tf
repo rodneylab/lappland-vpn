cacfb31b4bb87e74824f2ae61b44ede1b2465510
Rodney Lab Codesign <codesign@rodneylab.com>
2021-03-13 19:56:34 +0000

    fix(terraform/glcoud): 💫 updated image infrastructure code

--- terraform/gcloud/image/main.tf
+++ terraform/gcloud/image/main.tf
@@ -1,9 +1,14 @@
-variable "bucket" {}
+variable "bucket" {
+  description = "The name of the bucket to create."
+  type        = string
+}
 variable "image" {}
 variable "image_file" {}
 variable "image_name" {}
-variable "image_family" { default = "openbsd-amd64-68" }
-variable "project_id" {}
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+}
 variable "region" {}
 
 terraform {
@@ -13,41 +18,50 @@ terraform {
       version = "3.58.0"
     }
   }
+  required_version = ">= 0.12"
+}
+
+resource "google_project_service" "service" {
+  for_each = toset([
+    "cloudresourcemanager.googleapis.com"
+  ])
+
+  service = each.key
+
+  project            = var.project_id
+  disable_on_destroy = false
 }
 
 resource "google_storage_bucket" "lappland-openbsd-images" {
-  name                        = var.bucket
-  location                    = "EUROPE-WEST2"
-  project                     = var.project_id
-  storage_class               = "STANDARD"
-  requester_pays              = true
+  name          = var.bucket
+  project       = var.project_id
+  location      = "EUROPE-WEST2"
+  storage_class = "STANDARD"
+  # requester_pays              = true
   uniform_bucket_level_access = true
   force_destroy               = true
   lifecycle_rule {
-    condition {
-      age = 60
-    }
     action {
       type = "Delete"
     }
+    condition {
+      age        = 60
+      with_state = "ANY"
+    }
   }
+
+  # iam_members = [{
+  #   role = "roles/storage.viewer"
+  #   # member = "user:example-user@example.com"
+  #   member = var.service_account
+  # }]
 }
 
 resource "google_storage_bucket_object" "lappand-vpn-image" {
+  bucket = var.bucket
   name   = var.image_file
   source = var.image
-  bucket = var.bucket
 }
-
-# resource "google_compute_image" "lappland_vpn_image" {
-#   name = var.image_name
-#   raw_disk {
-#     source = "gs://${var.bucket}/${var.image_file}"
-#     # sha1 = "003b5dca54c0931480a5e055659140b94cf87d76" hash bedfore extracting
-#   }
-#   family  = var.image_family
-#   project = var.project_id
-# }
 
 output "image_hash" {
   value = google_storage_bucket_object.lappand-vpn-image.md5hash
